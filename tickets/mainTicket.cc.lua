5d7197b467f5d2547386ad7855a386894f22f6e4
Pedro-Pessoa <pedro_santos_40@hotmail.com>
2020-07-05 22:36:53 -0300

    update to channel link at mainticket

--- tickets/mainTicket.cc.lua
+++ tickets/mainTicket.cc.lua
@@ -36,16 +36,16 @@
 {{if gt $Delay 3}} {{$AoD = 2}} {{end}}
 {{if eq $AoD 1}}
     {{scheduleUniqueCC $SchedueledCCID nil $realDelay $tn (sdict "alert" 2)}}
-    {{dbSet (toInt $tn) "ticket" (sdict "AoD" $AoD "Delay" (str $Delay) "pos" 1 "ticketID" $tn "userID" (str .User.ID) "mainMsgID" (str $id) "ticketCounter" (str 0) "duration" ($time.Add (toDuration (print $Delay "h30m"))) "ctime" $time "alert" 2 "creator" (userArg .User.ID))}}
+    {{dbSet (toInt $tn) "ticket" (sdict "channelID" .Channel.ID "AoD" $AoD "Delay" (str $Delay) "pos" 1 "ticketID" $tn "userID" (str .User.ID) "mainMsgID" (str $id) "ticketCounter" (str 0) "duration" ($time.Add (toDuration (print $Delay "h30m"))) "ctime" $time "alert" 2 "creator" (userArg .User.ID))}}
 {{else}}
     {{$3HoursAlert := sub $realDelay 10800}}
     {{scheduleUniqueCC $SchedueledCCID nil $3HoursAlert $tn (sdict "alert" 1)}}
-    {{dbSet (toInt $tn) "ticket" (sdict "AoD" $AoD "Delay" (str $Delay) "pos" 1 "ticketID" $tn "userID" (str .User.ID) "mainMsgID" (str $id) "ticketCounter" (str 0) "duration" ($time.Add (toDuration (print $Delay "h"))) "ctime" $time "alert" 1 "creator" (userArg .User.ID))}}
+    {{dbSet (toInt $tn) "ticket" (sdict "channelID" .Channel.ID "AoD" $AoD "Delay" (str $Delay) "pos" 1 "ticketID" $tn "userID" (str .User.ID) "mainMsgID" (str $id) "ticketCounter" (str 0) "duration" ($time.Add (toDuration (print $Delay "h"))) "ctime" $time "alert" 1 "creator" (userArg .User.ID))}}
 {{end}}
 {{with (dbGet 0 "ticketDisplay").Value}}
     {{$map := sdict .}}
     {{if lt (len .) 50}}
-        {{$map.Set $tn $TO}}
+        {{$map.Set (str $.Channel.ID) $TO}}
     {{else}}
         {{$pos := 0}}
         {{range $k, $v := .}}
@@ -53,7 +53,7 @@
             {{- if lt (toInt $k) $pos}} {{$pos = toInt $k}} {{end -}}
         {{end}}
         {{$map.Del $pos}}
-        {{$map.Set $tn $TO}}
+        {{$map.Set (str $.Channel.ID) $TO}}
     {{end}}
     {{dbSet 0 "ticketDisplay" $map}}
 {{else}}
@@ -77,5 +77,6 @@
     {{end}}
 {{end}}
 {{$desc := printf "%s - %-10s\n" "**TicketID**" "**Status**"}}
-{{range $arr}} {{- $desc = print $desc (printf (print "`#%06d` - `%-" (index . 1 | len) "s`\n") (index . 0 | toInt) (index . 1)) -}} {{end}}
+{{range $arr}} {{- $desc = print $desc (printf (print "<#%d> - `%-" (index . 1 | len) "s`\n") (index . 0 | toInt) (index . 1)) -}} {{end}}
 {{editMessage $masterChannel $displayMSGID (cembed "title" "Tickets Display" "color" (randInt 16777216) "description" $desc)}}
+
