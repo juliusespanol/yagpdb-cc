6e7a91587db6487200e83528fdc4e73b94b3acc8
Julius Espanol <juliusespanol27@gmail.com>
2021-02-05 01:02:23 +0800

    Updated setupTickets.cc.lua

--- tickets/setupTickets.cc.lua
+++ tickets/setupTickets.cc.lua
@@ -12,16 +12,16 @@
 */}}
 
 {{/* USER VARIABLES */}}
-
+ 
     {{/* Satff Roles */}}
-        {{$Admins := cslice 673258482211749917}} {{/* IDs of your ADMINs Roles. Leave the "cslice" here even if you have only 1 role */}}
-        {{$Mods := cslice 674429313097007106}} {{/* IDs of your MODs Roles. Leave the "cslice" here even if you have only 1 role */}}
-        {{$MentionRoleID := 647298324734541827}} {{/* Role to be mentioned when a new ticket is opened */}}
-
+        {{$Admins := cslice 774825466771013633}} {{/* IDs of your ADMINs Roles. Leave the "cslice" here even if you have only 1 role */}}
+        {{$Mods := cslice 774827351409557524}} {{/* IDs of your MODs Roles. Leave the "cslice" here even if you have only 1 role */}}
+        {{$MentionRoleID := 784844402920390697}} {{/* Role to be mentioned when a new ticket is opened */}}
+ 
     {{/* Open Message Info */}}
-        {{$msgOpenChannelID := 578976698931085333}} {{/* Channel ID where the msg to open tickets is at. THIS CHANNEL CANT BE IN THE SAME CATEGORY AS THE TICKETS!!!!! */}}
-        {{$msgID := 656755443506610176}} {{/* Message ID of the message the user has to react to open ticket */}}
-
+        {{$msgOpenChannelID := 782935662118567956}} {{/* Channel ID where the msg to open tickets is at. THIS CHANNEL CANT BE IN THE SAME CATEGORY AS THE TICKETS!!!!! */}}
+        {{$msgID := 784848284719513650}} {{/* Message ID of the message the user has to react to open ticket */}}
+ 
     {{/* EMOJIS - Emoji MUST be unicode characters, like the examples here. */}}
         {{$OpenEmoji := "📩"}}
         {{$CloseEmoji := "🔒"}}
@@ -32,29 +32,29 @@
         {{$SaveTranscriptEmoji := "📑"}}
         {{$ReOpenEmoji := "🔓"}}
         {{$DeleteEmoji := "⛔"}}
-
+ 
     {{/* Ticket Status */}}
         {{$ticketOpen := "Open"}} {{/* Status of an open ticket - CAN NOT HAVE ANY SPECIAL CHARACTERS OR SPACE */}}
         {{$ticketClose := "Closed"}} {{/* Status of a closed ticket - CAN NOT HAVE ANY SPECIAL CHARACTERS OR SPACE */}}
         {{$ticketSolving := "Solving"}} {{/* Status of an solving ticket - CAN NOT HAVE ANY SPECIAL CHARACTERS OR SPACE */}}
-
+ 
     {{/* Misc */}}
-        {{$CCID := 132}} {{/* ID of your "Range CC" */}}
-        {{$SchedueledCCID := 133}} {{/* ID of your "Schedueled CC" */}}
-        {{$masterTicketChannelID := 721766366776131663}} {{/* A channel ID where the status of ur tickets will be displayed (Further explained in the README) */}}
-        {{$Trc := 682036950999629879}} {{/* Channe ID to save transcripts */}}
-        {{$category := 682207474178195489}} {{/* Tickets category ID */}}
+        {{$CCID := 7}} {{/* ID of your "Range CC" */}}
+        {{$SchedueledCCID := 9}} {{/* ID of your "Schedueled CC" */}}
+        {{$masterTicketChannelID := 782931111872692244}} {{/* A channel ID where the status of ur tickets will be displayed (Further explained in the README) */}}
+        {{$Trc := 782935725679181855}} {{/* Channe ID to save transcripts */}}
+        {{$category := 782935336040005642}} {{/* Tickets category ID */}}
         {{$Delay := 24}} {{/* Delay (in hours) for a ticket to automatically be deleted if no messages are sent */}}
-
+ 
 {{/* END OF USER VARIABLES */}}
-
-
-
+ 
+ 
+ 
 {{/* ACTUAL CODE! DONT TOUCH */}}
 {{if not .ExecData}}
     {{$error := ""}}
     {{$guildRoles := cslice}} {{range .Guild.Roles}} {{$guildRoles = $guildRoles.Append .ID}} {{end}} {{$invalid := false}}
-
+ 
     {{if not $Admins}}
         {{$error = print $error "\n" "You need to set at least one admin role in the **$Admins** variable"}}
     {{else}}
@@ -65,8 +65,8 @@
             {{$error = print $error "\n" "One or more of the admins roles provided in **$Admins** are not valid roles."}}
         {{end}}
     {{end}} {{$invalid = false}}
-
-
+ 
+ 
     {{if not $Mods}}
         {{$error = print $error "\n" "You need to set at least one admin role in the **$Admins** variable"}}
     {{else}}
@@ -77,28 +77,28 @@
             {{$error = print $error "\n" "One or more of the mod roles provided in **$Mods** are not valid roles."}}
         {{end}}
     {{end}}
-
-
+ 
+ 
     {{if not (in $guildRoles $MentionRoleID)}}
         {{$error = print $error "\n" "The provided **$MentionRoleID** is not a valid role."}}
     {{end}}
-
-
+ 
+ 
     {{if not (getChannel $msgOpenChannelID)}}
         {{$error = print $error "\n" "The **$msgOpenChannelID** provided is not a valid channel."}}
     {{end}}
-
-
+ 
+ 
     {{if not (getMessage $msgOpenChannelID $msgID)}}
         {{$error = print $error "\n" print "The **$msgID** provided is not a valid msg, or is not in the <#" $msgOpenChannelID "> channel."}}
     {{end}}
-
-
+ 
+ 
     {{if or (reFind `[^a-zA-Z\d-]` $ticketOpen) (reFind `[^a-zA-Z\d-]` $ticketClose) (reFind `[^a-zA-Z\d-]` $ticketSolving)}}
         {{$error = "Incorrect setup.\n**$ticketOpen $ticketClose $ticketSolving** can **NOT** have special characters like `á` or even white spaces ` `.\nThey also have to be a single word."}}
     {{end}}
-
-
+ 
+ 
     {{if not (toInt $CCID)}}
         {{$error = print $error "\n" "**$CCID** provided must be an int."}}
     {{end}}
@@ -106,8 +106,8 @@
     {{if not (reFind `This is the "Range CC" command.` $s)}}
         {{$error = print $error "\n" "**$CCID** provided is not a valid CC"}}
     {{end}}
-
-
+ 
+ 
     {{if not (toInt $SchedueledCCID)}}
         {{$error = print $error "\n" "**$SchedueledCCID** provided must be an int."}}
     {{end}}
@@ -115,23 +115,23 @@
     {{if not (reFind `This is the "Schedueled CC" command.` $s)}}
         {{$error = print $error "\n" "**$SchedueledCCID** provided is not a valid CC"}}
     {{end}}
-
-
+ 
+ 
     {{if not (getChannel $masterTicketChannelID)}}
         {{$error = print $error "\n" "The **$masterTicketChannelID** provided is not a valid channel."}}
     {{end}}
-
-
+ 
+ 
     {{if not (getChannel $Trc)}}
         {{$error = print $error "\n" "The **$Trc** provided is invalid. You have to set a proper channel to save transcripts."}}
     {{end}}
-
-
+ 
+ 
     {{if not (getChannel $category)}}
         {{$error = print $error "\n" "The **$category** provided is not a valid category."}}
     {{end}}
-
-
+ 
+ 
     {{if $Delay}}
         {{if ne (printf "%T" $Delay) "int"}}
             {{$error = print $error "\n" "The variable **$Delay** has to be an integer. i.e 1, 2, 3, etc...\nIt cannot be 2.75 for example"}}
@@ -139,8 +139,8 @@
             {{$error = print $error "\n" "The variable **$Delay** can not be less than 1."}}
         {{end}}
     {{end}}
-
-
+ 
+ 
     {{if not $error}}
         {{addReactions $OpenEmoji $CloseEmoji $SolveEmoji $AdminOnlyEmoji $ConfirmCloseEmoji $CancelCloseEmoji $SaveTranscriptEmoji $ReOpenEmoji $DeleteEmoji}}
     {{end}}
@@ -180,6 +180,6 @@ All good! If you did everything right, you should now be good to use your Reacti
 **TicketSolvingChannelStatus:** {{$setup.ticketSolving}}
 **TicketCloseChannelStatus:** {{$setup.ticketClose}}
 **Delay (in hours):** {{toInt $setup.Delay}}
-{{deleteResponse 120}}
+{{deleteResponse 1}}
 {{end}}
 {{deleteTrigger 7}}
